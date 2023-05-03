1aea9141442ef7f0e318061db35f139710453ed8
obed300 <obamfo@st.ug.edu.gh>
2023-05-03 07:13:32 +0000

    Add content

--- decorate.rb
+++ decorate.rb
@@ -19,6 +19,11 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    super.slice(0, 10)
+    name = super.correct_name
+    if name.length > 10
+      name[0, 10]
+    else
+      name
+    end
   end
 end
