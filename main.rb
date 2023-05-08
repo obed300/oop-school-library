76b5ee9479fe43d7cc2c5f4a92200a2961094113
obed300 <obamfo@st.ug.edu.gh>
2023-05-08 10:04:06 +0000

    Add content

--- main.rb
+++ main.rb
@@ -12,7 +12,6 @@ class Main
 
   def menu_options
     "
-M E N U
 1 - List all books
 2 - List all people
 3 - Create a person
@@ -40,15 +39,15 @@ Please choose an option by entering a number between 1 and 7: "
       when 6
         app.list_rentals
       else
-        exit
+        break
       end
     end
+    puts 'Thank you for using this app!'
   end
 
   def main
     puts "Welcome to the School Library app!\n"
     menu
-    puts 'Thank you for using this app!'
   end
 end
 
