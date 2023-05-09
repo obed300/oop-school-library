34c3015192cc6e5b283718b8a43ed67a5cbac627
obed300 <obamfo@st.ug.edu.gh>
2023-05-09 09:01:13 +0000

    Add content

--- apps.rb
+++ apps.rb
@@ -16,6 +16,49 @@ class App
 
   include InputModule
 
+  def display_options
+    "
+1 - List all books
+2 - List all people
+3 - Create a person
+4 - Create a book
+5 - Create a rental
+6 - List all rentals for a given person id
+7 - Exit
+Please choose an option by entering a number between 1 and 7: "
+  end
+
+  def options(input)
+    case input
+    when 1
+      list_books
+    when 2
+      print_person
+    when 3
+      add_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    else
+      puts 'You put the wrong input. Please enter a number between 1 and 7.'
+    end
+  end
+
+  def act_regarding_input
+    loop do
+      display_options
+      choice = gets.to_i
+      if choice == 7
+        puts 'Thank You for using my School Library!'
+        break
+      end
+      options(choice)
+    end
+  end
+
   def add_person
     text = 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     type = numeric_input(text, (1..2))
@@ -42,13 +85,10 @@ class App
   end
 
   def print_person
-    return 'No people to list' if @persons.empty?
-
-    result = ''
-    @persons.each_with_index do |person, index|
-      result += "#{index}) [#{person.class.name}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}\n"
+    puts 'People list is empty! Add a person.' if @persons.empty?
+    @persons.map do |person|
+      p "#{person.class.name} Name: #{person.name}, Age: #{person.age}, id: #{person.id}"
     end
-    print result
   end
 
   def create_book
@@ -62,39 +102,27 @@ class App
   end
 
   def list_books
-    i = 0
-    while i < @books.length
-      puts "#{i}) Title: #{@books[i].title}, Author: #{@books[i].author}"
-      i += 1
+    puts 'Book list is empty! Add a book.' if @books.empty?
+    @books.map do |book|
+      puts %(Title: "#{book.title}", Author: #{book.author})
     end
   end
 
   def create_rental
-    select_books = 'Select a book from the following list by number '
-    puts select_books
-    list_books
+    puts 'Select a book from the following list by number:'
+    @books.each_with_index { |book, index| puts %(#{index}\) Title: "#{book.title}", Author: #{book.author}) }
     book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not id)'
-    print_person
-    person_index = gets.chomp.to_i
-    print 'Date: '
 
-    date = gets.chomp.to_i
-    person = @persons[person_index]
-    book = @books[book_index]
-
-    @rentals << Rental.new(date, book, person)
-    puts 'Rental created sucessfully'
-  end
-
-  def person_by_id
-    loop
-    print 'ID of person: '
-    id = gets.chomp.to_i
-
-    @people.each_with_index do |person, index|
-      return index if person.id == id
+    @persons.each_with_index do |person, index|
+      puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+    person_index = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_i
+
+    @rentals << Rental.new(date, @books[book_index], @persons[person_index])
+    puts 'Rental created sucessfully'
   end
 
   def list_rentals
@@ -106,8 +134,9 @@ class App
       @persons.each do |person|
         puts "ID : #{person.id} => #{person.name}"
       end
-      puts "\n"
-      print "Enter person's ID :"
+      puts "
+
+    Enter person's ID :"
       person = gets.chomp
       puts "\n"
       @rentals.each do |rental|
--- main.rb
+++ main.rb
@@ -2,52 +2,56 @@ require './apps'
 require './input_module'
 
 class Main
-  attr_reader :app
+  def main
+    app = App.new
 
-  def initialize
-    @app = App.new
-  end
+    puts 'Welcome to school Library App'
+    puts ' '
 
-  include InputModule
-
-  def menu_options
-    "
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental
-6 - List all rentals for a given person id
-7 - Exit
-Please choose an option by entering a number between 1 and 7: "
-  end
-
-  def menu
     loop do
-      input = numeric_input(menu_options, (1..7))
-      case input
-      when 1
-        app.list_books
-      when 2
-        app.print_person
-      when 3
-        app.add_person
-      when 4
-        app.create_book
-      when 5
-        app.create_rental
-      when 6
-        app.list_rentals
-      else
-        break
+      display_options
+      option = gets.chomp.to_i
+
+      options(option, app)
+
+      if option == 7
+        puts 'Thanks you for using this app'
+        exit
       end
     end
-    puts 'Thank you for using this app!'
   end
 
-  def main
-    puts "Welcome to the School Library app!\n"
-    menu
+  def display_options
+    puts ' '
+    puts 'Please choose an option by entering a number: '
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a persons'
+    puts '4 - Create a book'
+    puts '5 - Create a rentals'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    puts ' '
+    puts 'Enter number: '
+  end
+
+  def options(option, app)
+    case option
+    when 1
+      app.list_books
+    when 2
+      app.print_person
+    when 3
+      app.add_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.list_rentals
+    else
+      puts 'Please enter a number between 1 and 7.'
+    end
   end
 end
 
